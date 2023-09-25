8bae5acf50984890fd94346d6f97c90de15f82a3
Your Name <you@example.com>
2023-09-25 19:39:14 +0545

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
-import back from './assets/img2.jpg'
+import back from './assets/img1.jpg'
+import back1 from './assets/img2.jpg'
 import { BsSearch } from 'react-icons/bs'
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
 
@@ -17,9 +18,21 @@ function App() {
 
 
   }
+  const myStyle1 = {
+    backgroundImage: `url(${back1})`,
+    height: '100vh',
+    backgroundSize: '100vw 100vh',
+    backgroundRepeat: 'no-repeat',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundAttachment: "fixed",
+
+
+  }
   return (
     <>
-      <div className="contain">
+      <section className="contain">
         <div className="container" style={myStyle}>
           <div className="title">
             <Link to='/' style={{ textDecoration: "none", color: "wheat", fontSize: "19px" }}>Home</Link>
@@ -34,18 +47,44 @@ function App() {
             <button><BsSearch /></button>
           </div>
         </div>
-      </div>
-
-      <div className="tit">
-        <h1 style={{color:"white",fontSize:"50px",wordSpacing:"10px"}}>Explore the world</h1>
-        <div className="para" >
-          <p style={{justifyContent:"center",position:"relative",left:"200px",color:"wheat",fontSize:"15px"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Sed dicta <br /> quo saepe  ad facilis ducimus porro at magni possimus quaerat?</p>
-          <p style={{justifyContent:"center",position:"relative",right:"220px",color:"wheat",fontSize:"15px"}}>Explore more <BsFillArrowRightCircleFill style={{justifyContent:"center",alignItems:"center"}}/> </p>
+        <div className="tit">
+          <div className="head">
+            <h1 style={{ color: "white", fontSize: "50px", wordSpacing: "10px" }}>Explore the world</h1>
+          </div>
+          <div className="para" >
+            <p style={{ justifyContent: "center", position: "relative", left: "200px", color: "wheat", fontSize: "15px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Sed dicta <br /> quo saepe  ad facilis ducimus porro at magni possimus quaerat?</p>
+            <p style={{ justifyContent: "center", position: "relative", right: "220px", color: "wheat", fontSize: "15px" }}>Explore more <BsFillArrowRightCircleFill style={{ justifyContent: "center", alignItems: "center" }} /> </p>
+          </div>
         </div>
-      </div>
+      </section>
 
-
+      <section className="about" style={myStyle1}>
+        <div className="container">
+          <div className="title">
+            <Link to='/' style={{ textDecoration: "none", color: "wheat", fontSize: "19px" }}>Home</Link>
+          </div>
+          <div className="title" style={{ position: "relative", left: "1rem" }}>
+            <Link to='/' className="main">About Us</Link>
+            <Link to='/' className="main"> Blog</Link>
+            <Link to='/' className="main">Contact Us</Link>
+          </div>
+          <div className="title" style={{ position: "relative", left: "200px" }}>
+            <input type="text" placeholder=".............." className="inp" />
+            <button><BsSearch /></button>
+          </div>
+        </div>
+        <div className="tit">
+          <div className="head">
+            <h1 style={{ color: "white", fontSize: "50px", wordSpacing: "10px" }}>Explore the world</h1>
+          </div>
+          <div className="para" >
+            <p style={{ justifyContent: "center", position: "relative", left: "200px", color: "wheat", fontSize: "15px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Sed dicta <br /> quo saepe  ad facilis ducimus porro at magni possimus quaerat?</p>
+            <p style={{ justifyContent: "center", position: "relative", right: "220px", color: "wheat", fontSize: "15px" }}>Explore more <BsFillArrowRightCircleFill style={{ justifyContent: "center", alignItems: "center" }} /> </p>
+          </div>
+        </div>
+      </section>
 
 
     </>
